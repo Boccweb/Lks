b5f1d0ab1fcc23cd02069d7d57c2b548f82994ba
Boccweb <penghancurmimpiriceshower@gmail.com>
2026-02-04 09:38:54 +0700

    Add files via upload

---
+++ dimas_CRUD/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD sederhana</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="navbar">
+    </div>
+    <div class="sidebar">
+        <div class="logo">
+            <a href="">smk<span>bisa</span>
+        </div>
+        <div class="menu-sidebar">
+            <ul>
+                <li class="active"><a href="index.php">dasboard</a></li>
+            </ul>
+            <ul>
+                <li><a href="daftariswa.php">daftar siswa</a></li>
+            </ul>
+            <ul>
+                <li><a href="">setting</a></li>
+            </ul>
+        </div>
+    </div>
+    <h1>selamat datang</h1>
+</body>
+</html>
---
+++ dimas_CRUD/koneksi.php
@@ -0,0 +1,8 @@
+<?php 
+   $host = "lOcalhost";
+   $user = "root";
+   $pass = "";
+   $db = "data_siswa";
+   $conn = mysqli_connect($host, $user, $pass< $db);
+
+?>
